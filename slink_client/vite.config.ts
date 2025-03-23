a913d223f08b5479519096dd3ec96af09f782a9b
Dax Harris <mvh3875@rit.edu>
2025-03-23 14:47:06 +0000

    switch to mantine for simplicity

--- slink_client/vite.config.ts
+++ slink_client/vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
 import { readFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
     https: {
